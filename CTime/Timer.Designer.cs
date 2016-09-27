33a20d905cd1b7012deea673c47babf49d3dc47e
CSirk <csirk005@gmail.com>
2016-09-26 21:39:59 -0500

    added Timer.cs with code and UI

--- CTime/Timer.Designer.cs
+++ CTime/Timer.Designer.cs
@@ -29,11 +29,243 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.aboveTimerNameLabel = new System.Windows.Forms.Label();
+            this.timerNameTextBox = new System.Windows.Forms.TextBox();
+            this.secondsDisplayLabel = new System.Windows.Forms.Label();
+            this.minutesDisplayLabel = new System.Windows.Forms.Label();
+            this.hoursDisplayLabel = new System.Windows.Forms.Label();
+            this.secondsNumeric = new System.Windows.Forms.NumericUpDown();
+            this.minutesNumeric = new System.Windows.Forms.NumericUpDown();
+            this.hoursNumeric = new System.Windows.Forms.NumericUpDown();
+            this.secondsAboveLabel = new System.Windows.Forms.Label();
+            this.minutesAboveLabel = new System.Windows.Forms.Label();
+            this.hoursAboveLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.countDownTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.secondsNumeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minutesNumeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hoursNumeric)).BeginInit();
+            this.SuspendLayout();
+            //
+            // progressBar1
+            //
+            this.progressBar1.Location = new System.Drawing.Point(148, 49);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(128, 10);
+            this.progressBar1.TabIndex = 35;
+            this.progressBar1.Visible = false;
+            //
+            // aboveTimerNameLabel
+            //
+            this.aboveTimerNameLabel.AutoSize = true;
+            this.aboveTimerNameLabel.Location = new System.Drawing.Point(145, 9);
+            this.aboveTimerNameLabel.Name = "aboveTimerNameLabel";
+            this.aboveTimerNameLabel.Size = new System.Drawing.Size(64, 13);
+            this.aboveTimerNameLabel.TabIndex = 34;
+            this.aboveTimerNameLabel.Text = "Timer Name";
+            //
+            // timerNameTextBox
+            //
+            this.timerNameTextBox.Location = new System.Drawing.Point(148, 25);
+            this.timerNameTextBox.Name = "timerNameTextBox";
+            this.timerNameTextBox.Size = new System.Drawing.Size(128, 20);
+            this.timerNameTextBox.TabIndex = 33;
+            //
+            // secondsDisplayLabel
+            //
+            this.secondsDisplayLabel.AutoSize = true;
+            this.secondsDisplayLabel.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondsDisplayLabel.Location = new System.Drawing.Point(88, 22);
+            this.secondsDisplayLabel.Name = "secondsDisplayLabel";
+            this.secondsDisplayLabel.Size = new System.Drawing.Size(54, 37);
+            this.secondsDisplayLabel.TabIndex = 32;
+            this.secondsDisplayLabel.Text = ".00";
+            this.secondsDisplayLabel.Visible = false;
+            //
+            // minutesDisplayLabel
+            //
+            this.minutesDisplayLabel.AutoSize = true;
+            this.minutesDisplayLabel.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minutesDisplayLabel.Location = new System.Drawing.Point(44, 22);
+            this.minutesDisplayLabel.Name = "minutesDisplayLabel";
+            this.minutesDisplayLabel.Size = new System.Drawing.Size(54, 37);
+            this.minutesDisplayLabel.TabIndex = 31;
+            this.minutesDisplayLabel.Text = ":00";
+            this.minutesDisplayLabel.Visible = false;
+            //
+            // hoursDisplayLabel
+            //
+            this.hoursDisplayLabel.AutoSize = true;
+            this.hoursDisplayLabel.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hoursDisplayLabel.Location = new System.Drawing.Point(7, 22);
+            this.hoursDisplayLabel.Name = "hoursDisplayLabel";
+            this.hoursDisplayLabel.Size = new System.Drawing.Size(47, 37);
+            this.hoursDisplayLabel.TabIndex = 30;
+            this.hoursDisplayLabel.Text = "00";
+            this.hoursDisplayLabel.Visible = false;
+            //
+            // secondsNumeric
+            //
+            this.secondsNumeric.Location = new System.Drawing.Point(104, 22);
+            this.secondsNumeric.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.secondsNumeric.Name = "secondsNumeric";
+            this.secondsNumeric.Size = new System.Drawing.Size(38, 20);
+            this.secondsNumeric.TabIndex = 29;
+            //
+            // minutesNumeric
+            //
+            this.minutesNumeric.Location = new System.Drawing.Point(60, 22);
+            this.minutesNumeric.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.minutesNumeric.Name = "minutesNumeric";
+            this.minutesNumeric.Size = new System.Drawing.Size(38, 20);
+            this.minutesNumeric.TabIndex = 28;
+            //
+            // hoursNumeric
+            //
+            this.hoursNumeric.Location = new System.Drawing.Point(15, 22);
+            this.hoursNumeric.Maximum = new decimal(new int[] {
+            23,
+            0,
+            0,
+            0});
+            this.hoursNumeric.Name = "hoursNumeric";
+            this.hoursNumeric.Size = new System.Drawing.Size(38, 20);
+            this.hoursNumeric.TabIndex = 27;
+            //
+            // secondsAboveLabel
+            //
+            this.secondsAboveLabel.AutoSize = true;
+            this.secondsAboveLabel.Location = new System.Drawing.Point(101, 9);
+            this.secondsAboveLabel.Name = "secondsAboveLabel";
+            this.secondsAboveLabel.Size = new System.Drawing.Size(14, 13);
+            this.secondsAboveLabel.TabIndex = 26;
+            this.secondsAboveLabel.Text = "S";
+            //
+            // minutesAboveLabel
+            //
+            this.minutesAboveLabel.AutoSize = true;
+            this.minutesAboveLabel.Location = new System.Drawing.Point(57, 9);
+            this.minutesAboveLabel.Name = "minutesAboveLabel";
+            this.minutesAboveLabel.Size = new System.Drawing.Size(16, 13);
+            this.minutesAboveLabel.TabIndex = 25;
+            this.minutesAboveLabel.Text = "M";
+            //
+            // hoursAboveLabel
+            //
+            this.hoursAboveLabel.AutoSize = true;
+            this.hoursAboveLabel.Location = new System.Drawing.Point(12, 9);
+            this.hoursAboveLabel.Name = "hoursAboveLabel";
+            this.hoursAboveLabel.Size = new System.Drawing.Size(15, 13);
+            this.hoursAboveLabel.TabIndex = 24;
+            this.hoursAboveLabel.Text = "H";
+            //
+            // startButton
+            //
+            this.startButton.BackColor = System.Drawing.Color.Black;
+            this.startButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.startButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.startButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.startButton.Location = new System.Drawing.Point(15, 67);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(83, 36);
+            this.startButton.TabIndex = 23;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = false;
+            //
+            // resetButton
+            //
+            this.resetButton.BackColor = System.Drawing.Color.Black;
+            this.resetButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.resetButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.resetButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
+            this.resetButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.resetButton.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.resetButton.Location = new System.Drawing.Point(193, 67);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(83, 36);
+            this.resetButton.TabIndex = 22;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = false;
+            //
+            // stopButton
+            //
+            this.stopButton.BackColor = System.Drawing.Color.Black;
+            this.stopButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.stopButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.stopButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
+            this.stopButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.stopButton.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stopButton.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.stopButton.Location = new System.Drawing.Point(104, 67);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(83, 36);
+            this.stopButton.TabIndex = 21;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = false;
+            //
+            // Timer
+            //
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 116);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.aboveTimerNameLabel);
+            this.Controls.Add(this.timerNameTextBox);
+            this.Controls.Add(this.secondsDisplayLabel);
+            this.Controls.Add(this.minutesDisplayLabel);
+            this.Controls.Add(this.hoursDisplayLabel);
+            this.Controls.Add(this.secondsNumeric);
+            this.Controls.Add(this.minutesNumeric);
+            this.Controls.Add(this.hoursNumeric);
+            this.Controls.Add(this.secondsAboveLabel);
+            this.Controls.Add(this.minutesAboveLabel);
+            this.Controls.Add(this.hoursAboveLabel);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.stopButton);
+            this.Name = "Timer";
+            this.Text = "Timer";
+            ((System.ComponentModel.ISupportInitialize)(this.secondsNumeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minutesNumeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hoursNumeric)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label aboveTimerNameLabel;
+        private System.Windows.Forms.TextBox timerNameTextBox;
+        private System.Windows.Forms.Label secondsDisplayLabel;
+        private System.Windows.Forms.Label minutesDisplayLabel;
+        private System.Windows.Forms.Label hoursDisplayLabel;
+        private System.Windows.Forms.NumericUpDown secondsNumeric;
+        private System.Windows.Forms.NumericUpDown minutesNumeric;
+        private System.Windows.Forms.NumericUpDown hoursNumeric;
+        private System.Windows.Forms.Label secondsAboveLabel;
+        private System.Windows.Forms.Label minutesAboveLabel;
+        private System.Windows.Forms.Label hoursAboveLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Timer countDownTimer;
     }
 }
 
--- CTime/Timer.cs
+++ CTime/Timer.cs
@@ -7,14 +7,148 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace CTime
 {
     public partial class Timer : Form
     {
+        [DllImport("user32.dll")]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+
+        public static int hours, minutes, seconds, incrementProgressBarCounter;
+        public static double numberOfSecondsForIncrementProgressBar;
+        public static Color hoursColor, minutesColor, secondsColor;
+
         public Timer()
         {
             InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+
+            this.Text = timerNameTextBox.Text;
+
+            hoursNumeric.Visible = minutesNumeric.Visible = secondsNumeric.Visible = timerNameTextBox.Visible = aboveTimerNameLabel.Visible = false;
+            hoursDisplayLabel.Visible = minutesDisplayLabel.Visible = secondsDisplayLabel.Visible = true;
+
+            hours = Convert.ToInt32(hoursNumeric.Value);
+            minutes = Convert.ToInt32(minutesNumeric.Value);
+            seconds = Convert.ToInt32(secondsNumeric.Value);
+
+            numberOfSecondsForIncrementProgressBar = ((hours * 360) + (minutes * 60) + seconds) / 100;
+
+            countDownTimer.Start();
+
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            countDownTimer.Stop();
+        }
+
+        private void countDownTimer_Tick(object sender, EventArgs e)
+        {
+            ChangeRemainingTime();
+            ChangeProgressBar();
+
+            hoursDisplayLabel.Text = ConvertTimeString(hours);
+            minutesDisplayLabel.Text = ":" + ConvertTimeString(minutes);
+            secondsDisplayLabel.Text = "." + ConvertTimeString(seconds);
+
+            hoursDisplayLabel.ForeColor = hoursColor;
+            minutesDisplayLabel.ForeColor = minutesColor;
+            secondsDisplayLabel.ForeColor = secondsColor;
+        }
+
+        public string ConvertTimeString(int timeAmount)
+        {
+            if (timeAmount < 10)
+            {
+                return "0" + timeAmount;
+            }
+            else
+            {
+                return timeAmount.ToString();
+            }
+        }
+
+        public void ChangeRemainingTime()
+        {
+            if (hours == 0 && minutes == 0 && seconds == 0)
+            {
+                countDownTimer.Stop();
+                var result = this.FindForm();
+                var processId = System.Diagnostics.Process.GetCurrentProcess();
+
+
+
+                var process = System.Diagnostics.Process.GetCurrentProcess();
+                IntPtr pointer = process.MainWindowHandle;
+                SetForegroundWindow(pointer);
+
+                MessageBox.Show("Timer : " + timerNameTextBox.Text + " has expired.");
+            }
+            else
+            {
+                if (seconds == 0)
+                {
+                    if (minutes == 0)
+                    {
+                        hours -= 1;
+                        minutes = 59;
+                        seconds = 59;
+                        hoursColor = Color.Red;
+                        minutesColor = Color.Blue;
+                        secondsColor = Color.Blue;
+
+                    }
+                    else
+                    {
+                        minutes -= 1;
+                        seconds = 59;
+                        hoursColor = Color.Blue;
+                        minutesColor = Color.Red;
+                        secondsColor = Color.Blue;
+                    }
+                }
+                else
+                {
+                    seconds -= 1;
+                    hoursColor = Color.Blue;
+                    minutesColor = Color.Blue;
+                    secondsColor = Color.Red;
+                }
+            }
+
+            hoursNumeric.Value = hours;
+            minutesNumeric.Value = minutes;
+            secondsNumeric.Value = seconds;
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            countDownTimer.Stop();
+            hoursNumeric.Visible = minutesNumeric.Visible = secondsNumeric.Visible = timerNameTextBox.Visible = aboveTimerNameLabel.Visible = true;
+            hoursDisplayLabel.Visible = minutesDisplayLabel.Visible = secondsDisplayLabel.Visible = false;
+            hoursNumeric.Value = minutesNumeric.Value = secondsNumeric.Value = 0;
+            hoursDisplayLabel.Text = minutesDisplayLabel.Text = secondsDisplayLabel.Text = "";
+            progressBar1.Value = 0;
+
+        }
+
+        public void ChangeProgressBar()
+        {
+            incrementProgressBarCounter++;
+
+            if (incrementProgressBarCounter >= numberOfSecondsForIncrementProgressBar)
+            {
+                progressBar1.Increment(1);
+                incrementProgressBarCounter = 0;
+            }
+
         }
     }
 }
